a2efce7d3bbb12a3647f4b410164b6ec1a827112
GuiMcL1990 <guirvasco@gmail.com>
2025-02-04 14:04:29 -0300

    Add files via upload

---
+++ Ativ_16.java
@@ -0,0 +1,13 @@
+package Atividade_For;
+
+import java.util.Scanner;
+
+public class Ativ_16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a;
+        System.out.println("Digite uma palavra: ");
+        a = sc.next();
+        System.out.println("O que você digitou foi "+a);
+    }
+}
